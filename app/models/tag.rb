df09e2565fc29cfaf1df31958a3158eb91c0734b
Micah Bowie <micahbowie20@gmail.com>
2022-01-22 10:48:12 -0500

    add association for organization pond and tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -22,6 +22,7 @@ class Tag < ApplicationRecord
   validates :name, length: { maximum: 25 }
 
   # Associations
+  belongs_to :organization
   has_and_belongs_to_many :ripples
 
   # Gem Configurations
