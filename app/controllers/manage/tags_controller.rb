5ddcab3e01a9776fd1953a4d90505cafefa2f269
Micah Bowie <micahbowie20@gmail.com>
2022-01-22 10:47:01 -0500

    update with organization

--- app/controllers/manage/tags_controller.rb
+++ app/controllers/manage/tags_controller.rb
@@ -4,9 +4,10 @@ module Manage
   class TagsController < ApplicationController
     before_action :admin_logged_in?
     before_action :set_tag, only: %i[show edit update destroy]
+    before_action :set_organizations, only: %i[new edit]
 
     def index
-      @tags = Tag.all
+      @tags = Tag.all.includes([:organization])
     end
 
     def show; end
@@ -22,7 +23,6 @@ module Manage
 
       if @tag.save
         redirect_to manage_tag_url(@tag), notice: 'Tag was successfully created.'
-
       else
         render :new, status: :unprocessable_entity
       end
@@ -49,8 +49,12 @@ module Manage
       @tag = Tag.friendly.find_by!(name: params[:name])
     end
 
+    def set_organizations
+      @organizations = Organization.all
+    end
+
     def tag_params
-      params.require(:tag).permit(:name, :description, :organization, :approved)
+      params.require(:tag).permit(:name, :description, :organization_id, :approved)
     end
   end
 end
