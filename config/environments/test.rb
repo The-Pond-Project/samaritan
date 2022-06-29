ff24f90e33cc34e7c5f43dddcbef0c814b6fea9b
DmitriiPetrovTR <dmitrii.petrov@thomsonreuters.com>
2022-06-29 09:16:02 +0800

    Fixed test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   end
 
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << /.*elb\.amazonaws\.com/
+  config.hosts << 'thepondproject.org'
+  config.hosts << IPAddr.new("10.0.0.0/16")
 
   config.cache_classes = true
 
