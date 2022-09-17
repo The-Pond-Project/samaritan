3552fb8fc57a8ff349e7bd7f6f3d82f970ebe541
Micah Bowie <67001260+micahbowie@users.noreply.github.com>
2022-09-17 14:50:53 -0400

    [HOTFIX] fix reverse address bug (#123)
    * fix reverse address bug
    
    * fix typo and safety navigator
    
    * rubocop

--- app/services/google/reverse_address.rb
+++ app/services/google/reverse_address.rb
@@ -15,17 +15,17 @@ module Google
     def initialize(latitude:, longitude:)
       @latitude = latitude
       @longitude = longitude
-      @response = call.parsed_response['results'].try(:first)
+      @response = call.parsed_response&.dig('results').try(:first)
     end
 
     ADDRESS_ATTRIBURES.each do |attr|
       define_method(attr.to_sym) do
         return nil unless response
 
-        value = response['address_components'].select do |x|
-          x['types'].include?(attr)
+        value = response&.dig('address_components')&.select do |x|
+          x&.dig('types')&.include?(attr)
         end.first
-        value['long_name']
+        value&.dig('long_name')
       end
     end
 
